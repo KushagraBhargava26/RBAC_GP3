8e9c22e2443670d09ad7591c3c974061785cf173
pashaarshad <arshadpashaintern@gmail.com>
2026-02-10 23:55:36 +0530

    ok

--- week 5/src/main.py
+++ week 5/src/main.py
@@ -37,7 +37,7 @@ app = FastAPI(title="RBAC Chatbot API")
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000", "http://127.0.0.1:3000"],
+    allow_origins=["*"],  # Allow all origins in development
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
